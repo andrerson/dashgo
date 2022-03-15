f6b226e0177a8c1f65b88da3e8c5c602be55aeb6
Anderson Freitas <andrersonn@gmail.com>
2022-03-15 13:15:20 -0300

    feat: finalizando dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -15,9 +15,9 @@ export function Header() {
     >
       <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="62">
         dashgo
-      </Text>
-      <Text as="span" ml="1" color="pink.500">
-        .
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
       </Text>
 
       <Flex
@@ -42,6 +42,36 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Anderson Freitas</Text>
+            <Text color="gray.300" fontSize="small">
+              andersonn@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Anderson Freitas"
+            src="https://github.com/andrerson.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
